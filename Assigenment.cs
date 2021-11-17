cfe1b7cfacb066dd1f3bdebc5e0ac3906e1f5e09
Kavitha-karthik <86833740+Kavitha-karthik@users.noreply.github.com>
2021-11-17 15:27:34 +0530

    Add files via upload

---
+++ Assigenment.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASynchronousProgram
+{
+    public class Assigenment
+    {
+        static void Main(string[] args)
+        {
+            SignDocumnetUsingDSAsync("Message for Asyn");
+            HashSantizedDocumentContentAsync("Hashed");
+            EncryptDigestUsingPrivateKeyAync("Encrypted");
+            Console.ReadLine();
+        }
+            static void SignDocumnetUsingDS(string documentContent)
+        {
+           string santizedDocContent= SantizeDocumentContent(documentContent);
+           string digest= HashSantizedDocumentContent(santizedDocContent);
+           string signedDocumentContent = EncryptDigestUsingPrivateKey(digest);
+            Console.WriteLine(signedDocumentContent);
+        }
+
+        static  async void SignDocumnetUsingDSAsync(string documentContent)
+        {
+            String content = await SantizeDocumentContentAync(documentContent);
+            Console.WriteLine("Content are ready");
+           
+
+        }
+
+       
+        private static Task<string> SantizeDocumentContentAync(string documentContent)
+        {
+            return Task.Run<string>(() =>
+            {
+            return SantizeDocumentContent(documentContent);
+            });
+        }
+        static string SantizeDocumentContent(string documentContent)
+        {
+            Task.Delay(3000).Wait();
+            Console.WriteLine("waiting to santized the content");
+            return documentContent + " Santized ";
+        }
+        static async void HashSantizedDocumentContentAsync(string HashDocumentContent)
+        {
+            String content = await HashSantizedDocumentContentAync(HashDocumentContent);
+            Console.WriteLine("Hashed are ready");
+
+
+        }
+
+
+        private static Task<string> HashSantizedDocumentContentAync(string documentContent)
+        {
+            return Task.Run<string>(() =>
+            {
+                return HashSantizedDocumentContent(documentContent);
+            });
+        }
+        static string HashSantizedDocumentContent(string HashDocumentContent)
+        {
+            Task.Delay(3000).Wait();
+            return HashDocumentContent + " Hashed ";
+        }
+
+        static async void EncryptDigestUsingPrivateKeyAync(string EncryDocumentContent)
+        {
+            String content = await EncryptDigestUsingPrivateKeyContent(EncryDocumentContent);
+            Console.WriteLine("Encrypted are ready");
+
+
+        }
+
+        private static Task<string> EncryptDigestUsingPrivateKeyContent(string EncryptedDocumentContent)
+        {
+            return Task.Run<string>(() =>
+            {
+                return EncryptDigestUsingPrivateKey(EncryptedDocumentContent);
+            });
+        }
+        
+        static string EncryptDigestUsingPrivateKey(string digest)
+        {
+            Task.Delay(3000).Wait();
+            return digest  + " Encrypted ";
+        }
+    }
+}
